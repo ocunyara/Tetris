4d667d7725c4cf22e18bd425334dc8f420997e58
Anton.Smolin <antonlezer@gmail.com>
2020-03-17 23:02:15 +0200

    Fix bugs in function. Create move function

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -13,18 +13,18 @@ export const usePlayer = () => {
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer(prev => ({
       ...prev,
-      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
       collided,
-    }));
-  };
+    }))
+  }
 
   const resetPlayer = useCallback(() => {
     setPlayer({
       pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
       tetromino: randomTetrominos().shape,
       collided: false,
-    });
-  }, []);
+    })
+  }, [])
 
   return [player, updatePlayerPos, resetPlayer];
-};
+}
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-
 import { createStage } from '../gameHalper';
 
 export const useStage = (player, resetPlayer) => {
@@ -8,29 +7,31 @@ export const useStage = (player, resetPlayer) => {
   useEffect(() => {
     const updateStage = prevStage => {
       // First flush the stage
-
       const newStage = prevStage.map(row =>
         row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
       );
 
-      // then drop the tetromino
-
+      // Then draw the tetromino
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
             newStage[y + player.pos.y][x + player.pos.x] = [
               value,
-              `${player.collided ? 'merged' : 'clean'}`,
+              `${player.collided ? 'merged' : 'clear'}`,
             ];
           }
         });
       });
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
 
       return newStage;
     };
 
     setStage(prev => updateStage(prev));
-  }, [player]);
+  }, [player, resetPlayer]);
 
   return [stage, setStage];
 };
